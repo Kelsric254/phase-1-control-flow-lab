fe509c8be053d392b991402b5a60671e2bba408c
Eric Kelvin Waweru <kelsric@yahoo.com>
2022-06-10 10:40:41 +0300

    assignment finished

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceInFeet){
+  if (distanceInFeet <= 400) {
+    return 'This one is on me!'
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else if (distanceInFeet > 2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destinationCity){
+  return destinationCity === 'NYC'?'Ok, sounds good.' : 'No go.'  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    case 'thanks for everything':
+      return 'Bye.'
+      break;
+  }
 }
